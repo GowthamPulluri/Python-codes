90d7b26f355c20f0e2da8a07fc144952fd9a6b72
GowthamPulluri <pullurigowtham@gmail.com>
2024-02-07 00:12:37 +0530

    adding python code

---
+++ Using variables.py
@@ -0,0 +1,14 @@
+name ="Sasi"
+object_1  ="Car"
+object_1_color ="Black"
+object_2 ="Office"
+cost ="10000"
+
+
+print("Hello " +name)
+print(name+", how is your day today?")
+print("Got to know that you bought a " +object_1+". Is it "+object_1_color)
+print("come to my " +object_2+ " someday")
+print("Thanks " +name)
+print("cost is "+cost)
+print("Bye")
