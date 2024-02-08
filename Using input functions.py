cdaa6a6f478de0269ec55b575669a776ef4d7d9d
GowthamPulluri <pullurigowtham@gmail.com>
2024-02-08 22:20:35 +0530

    Adding a python code of input funtions

---
+++ Using input functions.py
@@ -0,0 +1,7 @@
+name=input("What is your name? ")
+age=input("What is your age? ")
+education=input("What are you studying now? ")
+study=input("Where do you study? ")
+
+
+print("\n Hello, My name is "+name+". I am "+age+" years old. I am currently pursuing "+education+" in "+study)
