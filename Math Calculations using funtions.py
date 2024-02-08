e47c5fdfa9591611244481b8f1d48b18cfb5927e
GowthamPulluri <pullurigowtham@gmail.com>
2024-02-08 21:45:08 +0530

    add this python code

---
+++ Math Calculations using funtions.py
@@ -0,0 +1,17 @@
+# These are some math calculation  
+import math
+print(10+10)
+print((((10+10)*30)/3)-40)
+print(max(-2,66,49,4,9,-1)) # Biggest number
+print(min(-2,66,49,4,9,-1)) # Smallest number
+print(pow(6,4))  # 6 power 4
+print(32/43+pow(6,4))
+print(round(32/43+pow(6,4))) # It gives the round figure for +ve values
+print(-round(1/2-pow(6,4))) # This is for -ve values
+print(abs(-round(1/2-pow(6,4)))) # It gives the absolute values i.e. it gives even values 
+print(math.sqrt(48)) #square root value
+print(math.factorial(49))  #factorial
+print(math.sqrt(49))
+print(math.pi)
+print(math.cos(31)) # To find cosine values
+print("Factorial of 5:", math.factorial(5))
